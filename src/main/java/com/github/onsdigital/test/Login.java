7fbde96c955389b76db3566b3d516f672eb5c711
David Carboni <david@carboni.io>
2015-04-01 15:44:17 +0100

    Sorting out some kind of gitfail.

--- src/main/java/com/github/onsdigital/test/Login.java
+++ src/main/java/com/github/onsdigital/test/Login.java
@@ -20,24 +20,46 @@ public class Login {
     public static Host zebedeeHost;
 
     @Test
-    public void main() throws IOException {
-        zebedeeHost = new Host("http://localhost:8082");
+    public static void adminShouldBeAbleToLogIn() throws IOException {
+        loginAttempt("florence@magicroundabout.ons.gov.uk", "Doug4l", 200);
+    }
+
+    @Test
+    public static void return400IfEmailNotSpecified() throws IOException {
+        //given
+        loginAttempt(null, "password", 400);
+    }
+
+    @Test
+    public static void return401IfWrongPassword() throws IOException {
+        loginAttempt("florence@magicroundabout.ons.gov.uk", "denied", 401);
+    }
+
+
+    private static void loginAttempt(String email, String password, int expectedCode) throws IOException {
+        if (zebedeeHost == null) {
+            zebedeeHost = new Host("http://localhost:8082");
+        }
 
         Endpoint login = new Endpoint(zebedeeHost, "login");
         Http http = new Http();
 
-        Credentials credentials =new Credentials();
-        credentials.email="florence@magicroundabout.ons.gov.uk";
-        credentials.password = "Doug4l";
+        Credentials credentials = new Credentials();
+        credentials.email = email;
+        credentials.password = password;
 
         Response<String> response = http.post(login, credentials, String.class);
-        System.out.println("response = " + response);
+        System.out.println(response);
+        checkResponseCode(response, expectedCode);
 
-        florenceToken = response.body;
-
-
+        // if we managed to log in then make the other tests use us as the user
+        if (expectedCode == 200) {
+            florenceToken = response.body;
+        }
     }
 
-
+    private static void checkResponseCode(Response response, int code) {
+        org.junit.Assert.assertEquals(response.statusLine.getStatusCode(), code);
+    }
 
 }
