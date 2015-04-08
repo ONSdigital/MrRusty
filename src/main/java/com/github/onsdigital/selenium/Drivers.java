9f444702718633cbc545fda205ef531d9661755f
David Carboni <david@carboni.io>
2015-04-08 17:37:30 +0100

    Initial Selenium framework classes.

---
+++ src/main/java/com/github/onsdigital/selenium/Drivers.java
@@ -0,0 +1,84 @@
+package com.github.onsdigital.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * This class provides a way to access multiple {@link org.openqa.selenium.WebDriver}
+ * Created by david on 08/04/2015.
+ */
+public class Drivers {
+
+    /**
+     * @see <a href="https://ria101.wordpress.com/2011/12/12/concurrenthashmap-avoid-a-common-misuse/"
+     * >https://ria101.wordpress.com/2011/12/12/concurrenthashmap-avoid-a-common-misuse/</a>
+     */
+    static Map<String, WebDriver> drivers = java.util.Collections.synchronizedMap(new ConcurrentHashMap<String, WebDriver>(8, 0.9f, 1));
+
+
+    /**
+     * The default {@link DriverFactory} generates {@link HtmlUnitDriver} instances with Javascript enabled.
+     */
+    public static DriverFactory driverFactory = new DriverFactory() {
+        @Override
+        public WebDriver newDriver() {
+            return new HtmlUnitDriver(true);
+        }
+    };
+
+    /**
+     * If you want to use a different {@link WebDriver} setup, implement this interface and assign it to the {@link #driverFactory} field.
+     */
+    public interface DriverFactory {
+        WebDriver newDriver();
+    }
+
+    /**
+     * @param name A string to identify a particular driver.
+     * @return A {@link WebDriver} for the given name, creating it if necessary.
+     */
+    public static WebDriver get(String name) {
+        WebDriver driver = drivers.get(name);
+        if (driver == null) {
+            drivers.put(name, driver = driverFactory.newDriver());
+        }
+        return driver;
+    }
+
+    /**
+     * Convenience method to obtain a default {@link WebDriver}.
+     *
+     * @return A {@link WebDriver}. The same driver will be returned each time.
+     */
+    public static WebDriver get() {
+        return get("DEFAULT");
+    }
+
+    public static void quit() {
+        for (WebDriver driver : drivers.values()) {
+            driver.quit();
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // Last-chance attempt to ensure everything is cleaned up:
+        quit();
+    }
+
+//    makeAutoCloseable(WebDriver driver) {
+//        InvocationHandler handler = new InvocationHandler(){};
+//        MyInterface proxy = (MyInterface) Proxy.newProxyInstance(
+//                MyInterface.class.getClassLoader(),
+//                new Class[] { MyInterface.class },
+//                handler);
+//    }
+
+//    interface CloseableDriver extends WebDriver, AutoCloseable {
+//
+//    }
+
+}
---
+++ src/main/java/com/github/onsdigital/test/PageObjects/GoogleHomepage.java
@@ -0,0 +1,86 @@
+package com.github.onsdigital.test.PageObjects;
+
+import com.github.onsdigital.selenium.Drivers;
+import com.github.onsdigital.selenium.PageObject;
+import com.github.onsdigital.selenium.PageObjectException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.utils.URIBuilder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * Created by david on 08/04/2015.
+ */
+public class GoogleHomepage extends PageObject {
+
+    private static String searchBoxXpath = "//input[@type='text' and @name='q']";
+
+    public GoogleHomepage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void open() {
+        driver.get("http://google.com/ncr");
+    }
+
+    void openIfNecessary() {
+        String currentUrl = driver.getCurrentUrl();
+        URIBuilder builder = null;
+        try {
+            builder = new URIBuilder(currentUrl);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        if (!StringUtils.equalsIgnoreCase("google.com", builder.getHost())) {
+            open();
+        }
+    }
+
+    public GoogleResultPage search(String search) {
+
+        // Initial setup:
+        openIfNecessary();
+        checkSearchControls();
+
+        // Perform the search:
+        WebElement searchBox = driver.findElement(By.xpath(searchBoxXpath));
+        searchBox.sendKeys(search);
+        searchBox.submit();
+
+        // Return the next PageObject:
+        return new GoogleResultPage(driver);
+    }
+
+    private void checkSearchControls() {
+        List<WebElement> elements;
+
+        // Check the search box:
+        elements = driver.findElements(By.xpath(searchBoxXpath));
+        if (elements.size() != 1) {
+            String message;
+            if (elements.size() == 0) {
+                message = "Could not find the search box for XPath " + searchBoxXpath;
+            } else {
+                message = "Found " + elements.size() + " search boxes for XPath " + searchBoxXpath;
+            }
+            throw new PageObjectException(message);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            WebDriver driver = Drivers.get();
+            GoogleHomepage googleHomepage = new GoogleHomepage(driver);
+            GoogleResultPage resultPage = googleHomepage.search("David Carboni");
+            System.out.println(resultPage.getResults());
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("currentUrl = " + currentUrl);
+        } finally {
+            Drivers.quit();
+        }
+    }
+}
