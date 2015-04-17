f4b6f2404a1a67e842fc8eb9f961f0654d4875b6
CarlHembrough <carlhuk@gmail.com>
2015-04-17 12:12:31 +0100

    Adding check for a collection being returned in the collection.

--- src/main/java/com/github/onsdigital/test/api/Collections.java
+++ src/main/java/com/github/onsdigital/test/api/Collections.java
@@ -9,20 +9,38 @@ import com.github.onsdigital.http.Http;
 import com.github.onsdigital.http.Response;
 import com.github.onsdigital.http.Sessions;
 import com.github.onsdigital.junit.DependsOn;
-import org.junit.Assert;
+import com.github.onsdigital.zebedee.json.CollectionDescription;
+import com.github.onsdigital.zebedee.json.CollectionDescriptions;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @DependsOn({LoginAdmin.class, Collection.class})
 public class Collections {
+
+    private static Http http = Sessions.get("admin");
+
     @Test
     public void collectionsSpec() throws IOException {
-        Http http = Sessions.get("admin");
-        Endpoint endpoint = ZebedeeHost.collections;
-        Response<List> getResponse = http.get(endpoint, List.class);
 
-        Assert.assertEquals(getResponse.statusLine.getStatusCode(), 200);
+        // Given - an existing collection
+        CollectionDescription collection = Collection.create(http);
+
+        // When - we get the list of collections
+        Endpoint endpoint = ZebedeeHost.collections;
+        Response<CollectionDescriptions> getResponse = http.get(endpoint, CollectionDescriptions.class);
+
+        // Then - We get the existing collection in the response
+        assertEquals(getResponse.statusLine.getStatusCode(), 200);
+
+        for (CollectionDescription collectionDescription : getResponse.body) {
+            if (collection.name.equals(collectionDescription.name))
+                return;
+        }
+
+        fail("The collection was not found.");
     }
 }
