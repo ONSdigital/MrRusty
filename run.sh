e02ae2294fd7f56d46cde8842399ac414164bf22
CarlHembrough <carlhuk@gmail.com>
2015-08-06 15:35:55 +0100

    Update run script. No longer uses custom namespace param. Returns the return code given from tests.

--- run.sh
+++ run.sh
@@ -1,47 +1,26 @@
 #!/bin/bash
 
-# usage: ./run.sh [test namespace] [base url] [BrowserStack url]
-# e.g. ./run.sh browser localhost:8081  (run browser tests against localhost)
-#      ./run.sh api develop.carboni.uk  (run api tests against develop)
-# "http://carlhembrough2:FJCaeMyLNLBGxDVgACLp@hub.browserstack.com/wd/hub"
-
-# Remote debug:
-#export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n"
-#export PORT="8081"
-
-
-
 namespace=com.github.onsdigital
-
-# if the first argument has been passed parse as test namespace override
-if [ ! -z "$1" ]
-  then
-    namespace="${namespace}.test.$1"
-fi
 
 base_url="http://localhost:8081"
 florence_path="/florence/index.html"
 
+if [ ! -z "$1" ]
+  then
+    base_url="$1"
+fi
+
 if [ ! -z "$2" ]
   then
-    base_url="$2"
+    export BROWSERSTACK_URL="$2"
 fi
 
-if [ ! -z "$3" ]
-  then
-    export BROWSERSTACK_URL="$3"
-fi
-
-echo Running tests for namespace: ${namespace} against: ${base_url}
+echo Running tests against: ${base_url}
 
 export FLORENCE_URL="$base_url$florence_path"
 export ZEBEDEE_HOST="$base_url/zebedee"
 
 mvn clean package dependency:copy-dependencies && \
-java -jar target/*-jar-with-dependencies.jar ${namespace}
+java -jar target/*-jar-with-dependencies.jar
 
-
-#mvn clean package dependency:copy-dependencies && \
-#java -cp "target/classes:target/dependency/*" com.github.onsdigital.TestRunner
-
-#java $JAVA_OPTS -Drestolino.files=$RESTOLINO_STATIC -Drestolino.classes=$RESTOLINO_CLASSES -Drestolino.packageprefix=$RESTOLINO_PACKAGEPREFIX -cp "target/dependency/*" com.github.davidcarboni.restolino.Main
+exit $? # return the code from the last command.
