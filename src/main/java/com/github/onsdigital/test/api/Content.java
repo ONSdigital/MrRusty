57e5a67595366178b798ea5a7e40cabff05306d4
Thomas Ridd <twridd@gmail.com>
2015-08-06 11:46:36 +0100

    Directory call updated to account for all uri's starting with /

--- src/main/java/com/github/onsdigital/test/api/Content.java
+++ src/main/java/com/github/onsdigital/test/api/Content.java
@@ -198,7 +198,7 @@ public class Content {
         // And
         // A delete event should be present in the collection details
         CollectionDescription updatedCollection = Collection.get(collection.id, Login.httpPublisher).body;
-        assertTrue(updatedCollection.eventsByUri.get("/" + directory).hasEventForType(ContentEventType.DELETED));
+        assertTrue(updatedCollection.eventsByUri.get(directory).hasEventForType(ContentEventType.DELETED));
     }
 
     /**
