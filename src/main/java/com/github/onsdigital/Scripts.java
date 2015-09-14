01f202e1af285e8ad11269123d535d8b20458fe3
Thomas Ridd <twridd@gmail.com>
2015-09-14 12:38:50 +0100

    Added javadocs

--- src/main/java/com/github/onsdigital/Scripts.java
+++ src/main/java/com/github/onsdigital/Scripts.java
@@ -1,5 +1,6 @@
 package com.github.onsdigital;
 
+import com.github.onsdigital.test.api.Approve;
 import com.github.onsdigital.test.api.Collection;
 import com.github.onsdigital.test.api.Complete;
 import com.github.onsdigital.test.api.Review;
@@ -11,7 +12,6 @@ import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -20,11 +20,18 @@ import java.util.List;
  */
 public class Scripts {
 
+    /**
+     * Get a collection with a specified numbers of bulletins, datasets, and csdbFiles
+     *
+     * @param bulletins number of large bulletins (UK Environmental Accounts - 118 files)
+     * @param datasets number of spreadsheet datasets to include (zip file of size 60mb)
+     * @param csdbFiles number of csdb datasets to include (Blue Book - 3800 files)
+     * @return
+     * @throws Exception
+     */
     public static CollectionDescription buildCustomCollection(int bulletins, int datasets, int csdbFiles) throws Exception {
         OneShot.setup();
 
-
-        List<String> uris = new ArrayList<>();
         final Path bulletinSource = Paths.get("src/main/resources/script_resources/complex_bulletin");
         final Path datasetSource = Paths.get("src/main/resources/script_resources/big_dataset");
         final Path csdbSource = Paths.get("src/main/resources/script_resources/csdb_dataset");
@@ -100,6 +107,15 @@ public class Scripts {
         return Collection.get(collection.id, OneShot.httpPublisher).body;
     }
 
+    /**
+     * Get and bring to review stage a collection with a specified numbers of bulletins, datasets, and csdbFiles
+     *
+     * @param bulletins number of large bulletins (UK Environmental Accounts - 118 files)
+     * @param datasets number of spreadsheet datasets to include (zip file of size 60mb)
+     * @param csdbFiles number of csdb datasets to include (Blue Book - 3800 files)
+     * @return
+     * @throws Exception
+     */
     public static CollectionDescription buildReviewedCustomCollection(int bulletins, int datasets, int csdbFiles) throws Exception {
         CollectionDescription collection = Scripts.buildCustomCollection(bulletins, datasets, csdbFiles);
 
@@ -118,6 +134,17 @@ public class Scripts {
         return Collection.get(collection.id, OneShot.httpPublisher).body;
     }
 
+    /**
+     * Copy a resource to a temporary upload
+     *
+     * If a json file update its internals so that it references appropriate uris (just in case)
+     *
+     * @param fromPath
+     * @param replaceText <URI_ROOT>
+     * @param withText new root uri for this collection
+     * @return
+     * @throws IOException
+     */
     private static Path tempFileWithStringReplacement(Path fromPath,String replaceText, String withText) throws IOException {
         Path tmp = Files.createTempFile("temp", ".json");
         List<String> lines = Files.readAllLines(fromPath, Charset.forName("UTF8"));
@@ -131,7 +158,9 @@ public class Scripts {
         return tmp;
     }
 
+
     public static void main(String[] args) throws Exception {
-        buildReviewedCustomCollection(1, 1, 1);
+        CollectionDescription collection = buildReviewedCustomCollection(1, 1, 1);
+        Approve.approve(collection.id, OneShot.httpPublisher);
     }
 }
