9ae923382bba356e2ebf3253b3defc5d0c3163d2
CarlHembrough <carlhuk@gmail.com>
2015-12-09 12:12:16 +0000

    Update dataset tests to handle landing page + child pages.

--- src/main/java/com/github/onsdigital/test/api/Publish.java
+++ src/main/java/com/github/onsdigital/test/api/Publish.java
@@ -38,22 +38,27 @@ public class Publish {
      */
     @POST
     //@Test
-    public void dataPublisher_givenDataset_shouldPublishCSDBFiles() throws IOException {
+    public void dataPublisher_givenDataset_shouldPublishXls() throws IOException {
 
         // Given
         // a collection that we add files to and then
         CollectionDescription collection = OneLineSetups.publishedCollection();
 
-        File json = new File("src/main/resources/dummy_dataset/data.json");
-        File csdb = new File("src/main/resources/dummy_dataset/dummy_csdb.csdb");
+        File jsonParent = new File("src/main/resources/dummy_dataset/data.json");
+        File json = new File("src/main/resources/dummy_dataset/2015/data.json");
+        File csdb = new File("src/main/resources/dummy_dataset/2015/3a82caed.xls");
 
 
         String baseUri = "/archive/" + Random.id() + "/datasets/dataset";
-        Content.upload(collection.id, baseUri + "/data.json", json, Login.httpPublisher);
-        Content.upload(collection.id, baseUri + "/CXNV.csdb", csdb, Login.httpPublisher);
+        Content.upload(collection.id, baseUri + "/data.json", jsonParent, Login.httpPublisher);
+        Content.upload(collection.id, baseUri + "/2015/data.json", json, Login.httpPublisher);
+        Content.upload(collection.id, baseUri + "/2015/3a82caed.xls", csdb, Login.httpPublisher);
 
         Complete.complete(collection.id, baseUri + "/data.json", Login.httpPublisher);
+        Complete.complete(collection.id, baseUri + "/2015/data.json", Login.httpPublisher);
+
         Review.review(collection.id, baseUri + "/data.json", Login.httpSecondSetOfEyes);
+        Review.review(collection.id, baseUri + "/2015/data.json", Login.httpSecondSetOfEyes);
         Approve.approve(collection.id, Login.httpPublisher);
 
         // When
@@ -78,15 +83,20 @@ public class Publish {
         // a collection
         CollectionDescription collection = OneLineSetups.publishedCollection();
 
-        File json = new File("src/main/resources/dummy_timeseriesdataset/data.json");
-        File csdb = new File("src/main/resources/dummy_timeseriesdataset/dummy_csdb.csdb");
+        File jsonParent = new File("src/main/resources/dummy_timeseriesdataset/data.json");
+        File json = new File("src/main/resources/dummy_timeseriesdataset/2015/data.json");
+        File csdb = new File("src/main/resources/dummy_timeseriesdataset/2015/dummy_csdb.csdb");
 
         String baseUri = "/archive/" + Random.id() + "/datasets/timeseriesdataset";
-        Content.upload(collection.id, baseUri + "/data.json", json, Login.httpPublisher);
-        Content.upload(collection.id, baseUri + "/CXNV.csdb", csdb, Login.httpPublisher);
+        Content.upload(collection.id, baseUri + "/data.json", jsonParent, Login.httpPublisher);
+        Content.upload(collection.id, baseUri + "/2015/data.json", json, Login.httpPublisher);
+        Content.upload(collection.id, baseUri + "/2015/CXNV.csdb", csdb, Login.httpPublisher);
 
         assertOk(Complete.complete(collection.id, baseUri + "/data.json", Login.httpPublisher));
+        assertOk(Complete.complete(collection.id, baseUri + "/2015/data.json", Login.httpPublisher));
+
         assertOk(Review.review(collection.id, baseUri + "/data.json", Login.httpSecondSetOfEyes));
+        assertOk(Review.review(collection.id, baseUri + "/2015/data.json", Login.httpSecondSetOfEyes));
         assertOk(Approve.approve(collection.id, Login.httpPublisher));
 
         // When
