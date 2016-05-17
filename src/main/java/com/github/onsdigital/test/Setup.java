f4d89c09df7ed9d47d6fde30c9bbcff2d8a1c91d
CarlHembrough <carlhuk@gmail.com>
2016-05-17 09:22:17 +0100

    Initialise context object when setup is run.

--- src/main/java/com/github/onsdigital/test/Setup.java
+++ src/main/java/com/github/onsdigital/test/Setup.java
@@ -2,10 +2,11 @@ package com.github.onsdigital.test;
 
 public class Setup implements com.github.onsdigital.junit.Setup {
 
-    public static Context context = new Context();
+    public static Context context;
 
     @Override
     public void setup() throws Exception {
+        context = new Context();
         context.setup();
     }
 }
