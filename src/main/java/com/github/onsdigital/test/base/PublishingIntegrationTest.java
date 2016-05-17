58e883b227fb552a529e065aba796a2293970ece
CarlHembrough <carlhuk@gmail.com>
2016-05-17 08:51:58 +0100

    Do not do class based setup / teardown when running the full suite of tests. Do it before and after the suite. To be refactored when teardown code is related only to the class being run.

--- src/main/java/com/github/onsdigital/test/base/PublishingIntegrationTest.java
+++ src/main/java/com/github/onsdigital/test/base/PublishingIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.github.onsdigital.test.base;
 
 import com.github.onsdigital.test.Context;
+import com.github.onsdigital.test.Setup;
 import com.github.onsdigital.test.TearDownAfterTesting;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -8,13 +9,18 @@ import org.junit.BeforeClass;
 public abstract class PublishingIntegrationTest {
 
     private static boolean hasSetupRun = false;
-    private static boolean hasTeardownRun = false;
 
     protected static Context context = new Context();
 
     @BeforeClass
     public synchronized static void setup() throws Exception {
         System.out.println("Running test setup code...");
+
+        if (Setup.context != null) {
+            context = Setup.context;
+            return; // hack to skip the class teardown when running the full suite.
+        }
+
         if (!hasSetupRun) {
             hasSetupRun = true;
             context.setup();
@@ -24,6 +30,10 @@ public abstract class PublishingIntegrationTest {
     @AfterClass
     public static void teardown() {
         System.out.println("Running test teardown code...");
+
+        if (Setup.context != null)
+            return; // hack to skip the class teardown when running the full suite.
+
         TearDownAfterTesting.doTeardown(context);
     }
 }
